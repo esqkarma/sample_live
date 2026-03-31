1018a1582a98b7d34185a0006b4b980aee0a9a09
Vipin Karma S <vipinkarma5@gmail.com>
2026-03-31 10:48:13 +0530

    read from databse

--- myapp/views.py
+++ myapp/views.py
@@ -7,6 +7,7 @@ from .models import *
 
 @csrf_exempt
 def addproduct_get(request):
+    a = Product.objects.all()
     if request.method == 'POST':
         name = request.POST['name']
         price = request.POST['price']
@@ -14,4 +15,4 @@ def addproduct_get(request):
         p.name = name
         p.price = price
         p.save()
-    return render(request,'add_product.html')
+    return render(request,'add_product.html',{'data':a})
