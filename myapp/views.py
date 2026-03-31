d0387ed503b36f848ae9fccf115dd83ce40a3a8e
Vipin Karma S <vipinkarma5@gmail.com>
2026-03-31 10:31:36 +0530

    create operation

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def addproduct_get(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        price = request.POST['price']
+        p = Product()
+        p.name = name
+        p.price = price
+        p.save()
     return render(request,'add_product.html')
